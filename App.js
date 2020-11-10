b0caccb96aacf55672c9bdee49a79f23eb6be89a
naracodes <naralee0124@gmail.com>
2020-11-09 23:44:13 -0500

    add align items

--- App.js
+++ App.js
@@ -441,6 +441,95 @@
 //   }
 // });
 
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.containerOne}>
+
+//       </View>
+//       <View style={styles.containerTwo}>
+//         <View style={styles.innerOne}>
+
+//         </View>
+//         <View style={styles.innerTwo}>
+
+//         </View>
+
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex:1
+//   },
+//   containerOne: {
+//     flex:1,
+//     backgroundColor:"red"
+//   },
+//   containerTwo:{
+//     flex:2,
+//     backgroundColor:"yellow"
+//   },
+//   innerOne: {
+//     flex:1,
+//     backgroundColor:"blue"
+//   },
+//   innerTwo: {
+//     flex:4,
+//     backgroundColor:"orange"
+//   }
+// });
+
+// import React from 'react';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <View style={styles.containerOne}>
+
+//       </View>
+//       <View style={styles.containerTwo}>
+//         <View style={styles.innerOne}>
+
+//         </View>
+//         <View style={styles.innerTwo}>
+
+//         </View>
+
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex:1
+//   },
+//   containerOne: {
+//     flex:1,
+//     backgroundColor:"red"
+//   },
+//   containerTwo:{
+//     flex:2,
+//     flexDirection:"row",
+//     backgroundColor:"yellow"
+//   },
+//   innerOne: {
+//     flex:1,
+//     backgroundColor:"blue"
+//   },
+//   innerTwo: {
+//     flex:4,
+//     backgroundColor:"orange"
+//   }
+// });
+
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
@@ -452,10 +541,10 @@ export default function App() {
       </View>
       <View style={styles.containerTwo}>
         <View style={styles.innerOne}>
-
+         
         </View>
         <View style={styles.innerTwo}>
-
+          <Text>!!컨텐츠!!</Text>
         </View>
 
       </View>
@@ -473,6 +562,7 @@ const styles = StyleSheet.create({
   },
   containerTwo:{
     flex:2,
+    flexDirection:"row",
     backgroundColor:"yellow"
   },
   innerOne: {
@@ -481,6 +571,9 @@ const styles = StyleSheet.create({
   },
   innerTwo: {
     flex:4,
+    justifyContent:"center",
+    flexDirection:'row',
+    // alignItems:"center",
     backgroundColor:"orange"
   }
 });
