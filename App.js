abcbe05158de76d0a975dc73e0275daed028721b
naracodes <naralee0124@gmail.com>
2020-11-10 16:57:44 -0500

    render multiple tips using json

--- App.js
+++ App.js
@@ -559,13 +559,178 @@
 //   },
 // });
 
+// import React from 'react';
+// import main from './assets/main.png';
+// import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
+// import data from './data.json';
+// export default function App() {
+//   console.disableYellowBox = true;
+//   //return 구문 밖에서는 슬래시 두개 방식으로 주석
+
+//   let tip = data.tip;
+//   let todayWeather = 10 + 17;
+//   let todayCondition = "흐림"
+
+//   return (
+//     /*
+//       return 구문 안에서는 {슬래시 + * 방식으로 주석
+//     */
+//     <ScrollView style={styles.container}>
+//       <Text style={styles.title}>나만의 꿀팁</Text>
+//       <Text style={styles.weather}>오늘의 날씨: {todayWeather + '°C ' + todayCondition} </Text>
+//       <Image style={styles.mainImage} source={main}/>
+//       <ScrollView style={styles.middleContainer} horizontal indicatorStyle={"white"}>
+//         <TouchableOpacity style={styles.middleButton01}><Text style={styles.middleButtonText}>생활</Text></TouchableOpacity>
+//         <TouchableOpacity style={styles.middleButton02}><Text style={styles.middleButtonText}>재테크</Text></TouchableOpacity>
+//         <TouchableOpacity style={styles.middleButton03}><Text style={styles.middleButtonText}>반려견</Text></TouchableOpacity>
+//         <TouchableOpacity style={styles.middleButton04}><Text style={styles.middleButtonText}>꿀팁 찜</Text></TouchableOpacity>
+//       </ScrollView>
+//       <View style={styles.cardContainer}>
+//          {/* 하나의 카드 영역을 나타내는 View */}
+//          { 
+//           tip.map((content,i)=>{
+//             return (<View style={styles.card} key={i}>
+//               <Image style={styles.cardImage} source={{uri:content.image}}/>
+//               <View style={styles.cardText}>
+//                 <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
+//                 <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
+//                 <Text style={styles.cardDate}>{content.date}</Text>
+//               </View>
+//             </View>)
+//           })
+//          }
+//       </View>
+//     </ScrollView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     //앱의 배경 색
+//     backgroundColor: '#fff',
+//   },
+//   title: {
+//     //폰트 사이즈
+//     fontSize: 20,
+//     //폰트 두께
+//     fontWeight: '700',
+//     //위 공간으로 부터 이격
+//     marginTop:50,
+//     //왼쪽 공간으로 부터 이격
+//     marginLeft:20
+//   },
+//   weather:{
+//     alignSelf:"flex-end",
+//     paddingRight:20
+//   },
+//   mainImage: {
+//     //컨텐츠의 넓이 값
+//     width:'90%',
+//     //컨텐츠의 높이 값
+//     height:200,
+//     //컨텐츠의 모서리 구부리기
+//     borderRadius:10,
+//     marginTop:20,
+//     //컨텐츠 자체가 앱에서 어떤 곳에 위치시킬지 결정(정렬기능)
+//     //각 속성의 값들은 공식문서에 고대로~ 나와 있음
+//     alignSelf:"center"
+//   },
+//   middleContainer:{
+//     marginTop:20,
+//     marginLeft:10,
+//     height:60
+//   },
+//   middleButton01: {
+//     width:100,
+//     height:50,
+//     padding:15,
+//     backgroundColor:"#fdc453",
+//     borderColor:"deeppink",
+//     borderRadius:15,
+//     margin:7
+//   },
+//   middleButton02: {
+//     width:100,
+//     height:50,
+//     padding:15,
+//     backgroundColor:"#fe8d6f",
+//     borderRadius:15,
+//     margin:7
+//   },
+//   middleButton03: {
+//     width:100,
+//     height:50,
+//     padding:15,
+//     backgroundColor:"#9adbc5",
+//     borderRadius:15,
+//     margin:7
+//   },
+//   middleButtonText: {
+//     color:"#fff",
+//     fontWeight:"700",
+//     //텍스트의 현재 위치에서의 정렬 
+//     textAlign:"center"
+//   },
+//   middleButton04: {
+//     width:100,
+//     height:50,
+//     padding:15,
+//     backgroundColor:"#f886a8",
+//     borderRadius:15,
+//     margin:7
+//   },
+//   cardContainer: {
+//     marginTop:10,
+//     marginLeft:10
+//   },
+//   card:{
+//     flex:1,
+//     //컨텐츠들을 가로로 나열
+//     //세로로 나열은 column <- 디폴트 값임 
+//     flexDirection:"row",
+//     margin:10,
+//     borderBottomWidth:0.5,
+//     borderBottomColor:"#eee",
+//     paddingBottom:10
+
+//   },
+//   cardImage: {
+//     flex:1,
+//     width:100,
+//     height:100,
+//     borderRadius:10,
+//   },
+//   cardText: {
+//     flex:2,
+//     flexDirection:"column",
+//     marginLeft:10,
+//   },
+//   cardTitle: {
+//     fontSize:20,
+//     fontWeight:"700"
+//   },
+//   cardDesc: {
+//     fontSize:15
+//   },
+//   cardDate: {
+//     fontSize:10,
+//     color:"#A6A6A6",
+//   },
+
+
+// });
+
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 import mainPhoto from "./assets/main.png"
 import pizzaPhoto from "./assets/pizza.png"
+import data from './data.json';
 
 
 export default function App() {
+
+  const tips = data.tips;
+
   return (
     <ScrollView contentContainerStyle={styles.container}>
       <View style={styles.textContainer}><Text style={styles.textHeader}>&#9816;&#10084;</Text></View>
@@ -584,25 +749,30 @@ export default function App() {
           <TouchableOpacity style={styles.categoryButtons4}><Text style={styles.categoryText}>Saved</Text></TouchableOpacity>
         </ScrollView>
         <View style={styles.noteContainer}>
-          <View style={styles.singleNoteContainer}>
-            <View style={styles.pizzaContainer}>
-              <Image
-                source={pizzaPhoto}
-                resizeMode="cover"
-                style={styles.pizzaImage}
-              />
-            </View>
-            <View style={styles.pizzaTextContainer}>
-              <Text h3 style={styles.pizzaTitle}>먹다 남은 피자를 촉촉하게!</Text>
-              <Text numberOfLines={3} style={styles.pizzaDescription}>
-                먹다 남은 피자는 수분이 날라가기 때문에 처음처럼 맛있게 먹을 수 없는데요. 이럴 경우 그릇에 물을 받아 전자레인지 안에서 1분 30초에서 2분 정도 함께 돌려주면 촉촉하게 먹을 수 있습니다. 물이 전자레인지 안에서 수증기를 일으키고, 피자에 촉촉함을 더해줍니다.
-              </Text>
-              <Text style={styles.noteDate}>09-09-2020</Text>
-            </View>
+            {
+              tips.map((content, i) => {
+                return (
+                  <View style={styles.singleNoteContainer} key={i}>
+                    <View style={styles.pizzaContainer}>
+                      <Image
+                        // source={pizzaPhoto}
+                        source={{uri: content.image}}
+                        resizeMode="cover"
+                        style={styles.pizzaImage}
+                      />
+                    </View>
+                    <View style={styles.pizzaTextContainer}>
+                      <Text h3 style={styles.pizzaTitle}>{content.title}</Text>
+                      <Text numberOfLines={3} style={styles.pizzaDescription}>
+                        {content.desc}
+                      </Text>
+                      <Text style={styles.noteDate}>{content.date}</Text>
+                    </View>
 
-          </View>
-          <View style={styles.singleNoteContainer}><Text></Text></View>
-          <View style={styles.singleNoteContainer}><Text></Text></View>
+                  </View>
+                );
+              })
+            }
         </View>
       </View>
     </ScrollView>
@@ -617,29 +787,26 @@ function pickColor() {
 
 const styles = StyleSheet.create({
   container: {
-    flex:1,
     // backgroundColor:"#d9818f",
     paddingLeft: 15,
     paddingRight: 15,
   },
   textContainer: {
-    flex: 1,
     // backgroundColor: "gray",
     justifyContent: "flex-end",
   },
   textHeader: {
     fontSize: 50,
     paddingBottom: 5,
+    marginTop: 12,
     // paddingLeft: 8,
   },
   contentContainer: {
-    flex: 10,
     // backgroundColor: "#adbae6"
   },
   imageContainer: {
-    marginTop:2,
-    marginBottom:2,
-    flex: 4,
+    width: "100%",
+    height: 180,
   },
   image: {
     width:"100%",
@@ -647,11 +814,9 @@ const styles = StyleSheet.create({
     borderRadius:20,
   },
   categoryContainer: {
-    width:"100%",
-    backgroundColor: "white",
-    flex:1,
+    marginTop: 10,
+    height: 60,
     flexDirection: "row",
-    justifyContent: "space-evenly",
     alignItems: "center",
   },
   categoryButtons1: {
@@ -660,7 +825,7 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: "center",
     justifyContent: "center",
-    marginRight: 50,
+    marginRight: 15,
     backgroundColor: pickColor(),
   },
   categoryButtons2: {
@@ -669,7 +834,7 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: "center",
     justifyContent: "center",
-    marginRight: 50,
+    marginRight: 15,
     backgroundColor: pickColor(),
   },
   categoryButtons3: {
@@ -678,7 +843,7 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: "center",
     justifyContent: "center",
-    marginRight: 50,
+    marginRight: 15,
     backgroundColor: pickColor(),
   },
   categoryButtons4: {
@@ -687,7 +852,7 @@ const styles = StyleSheet.create({
     width: 100,
     alignItems: "center",
     justifyContent: "center",
-    marginRight: 50,
+    // marginRight: 50,
     backgroundColor: pickColor(),
   },
   categoryText: {
@@ -696,8 +861,7 @@ const styles = StyleSheet.create({
   },
   noteContainer: {
     flex: 6,
-    // backgroundColor: "purple",
-    // flexDirection: "row",
+    marginTop: 10,
   },
   singleNoteContainer: {
     flex: 1,
@@ -714,8 +878,11 @@ const styles = StyleSheet.create({
   },
   pizzaTextContainer: {
     flex: 2,
-    padding: 10,
-    justifyContent: "flex-start"
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 10,
+    // paddingTop: 10,
+    // marginLeft: 10,
   },
   pizzaText: {
     flex:2,
