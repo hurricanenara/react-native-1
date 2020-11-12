ab58944c111a13a46723928485f599d794b00eb0
naracodes <naralee0124@gmail.com>
2020-11-12 00:36:58 -0500

    add navigator to navigate to detailed page

--- components/detail.js
+++ components/detail.js
@@ -1,76 +1,376 @@
-import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
-// import LinearGradient from 'react-native-linear-gradient'
-import { LinearGradient } from "expo-linear-gradient";
-import { Animated } from "react-native";
+// import React from 'react';
+// import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+// // import LinearGradient from 'react-native-linear-gradient'
+// import { LinearGradient } from "expo-linear-gradient";
+// import { Animated } from "react-native";
 
-export default function Detail() {
+// export default function Detail() {
 
-    const tip = {
+//     const tip = {
+//         "idx":9,
+//         "category":"재테크",
+//         "title":"렌탈 서비스 금액 비교해보기",
+//         "image": "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Frental.png?alt=media&token=97a55844-f077-4aeb-8402-e0a27221570b",
+//         "desc":"요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
+//         "date":"2020.09.09"
+//     };
+
+//     function toggleColor() {
+//         // styles.container.backgroundColor === { color: "white" } ? { color: "black" } : { color: "white" };
+//         return null;
+//     }
+
+//     return (
+//         <ScrollView contentContainerStyle={styles.container}>
+//             <Image 
+//                 source={{uri: tip.image}}
+//                 style={styles.mainImage}
+//             />
+//             <Text style={Object.assign({}, styles.detailTitle, toggleColor())}>{tip.title}</Text>
+//             {/* <Text style={styles.detailTitle}>{styles.container.backgroundColor}</Text> */}
+//             <Text style={styles.detailDescription}>{tip.desc}</Text>
+//             <TouchableOpacity style={styles.tipButton}><Text style={styles.tipButtonText}>팁 찜하기</Text></TouchableOpacity>
+//         </ScrollView>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         backgroundColor: "white",
+//         flex: 1,
+//     },
+//     mainImage: {
+//         width: "100%",
+//         height: "50%",
+//         marginTop: 22,
+//     },
+//     detailTitle: {
+//         // color: "black",
+//         alignSelf: "center",
+//         fontSize: 22,
+//         fontWeight: "700",
+//         margin: 22,
+//     },
+//     detailDescription: {
+//         // color: "black",
+//         width: "90%",
+//         alignSelf: "center",
+//         lineHeight: 20,
+//     },
+//     tipButton: {
+//         borderColor: "pink",
+//         borderWidth: 1,
+//         borderStyle: "solid",
+//         width: "28%",
+//         alignSelf: "center",
+//         margin: 28,
+//         borderRadius: 5
+//     },
+//     tipButtonText: {
+//         // color: "black",
+//         textAlign: "center",
+//         padding: 10
+//     },
+
+// });
+
+// import React,{useState,useEffect} from 'react';
+// import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity,Alert } from 'react-native';
+
+// export default function Detail({navigation,route}) {
+                
+//                 //초기 컴포넌트의 상태값을 설정
+//                 //state, setState 뿐 아니라 이름을 마음대로 지정할 수 있음!
+//     const [tip, setTip] = useState({
+//         "idx":9,
+//         "category":"재테크",
+//         "title":"렌탈 서비스 금액 비교해보기",
+//         "image": "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Frental.png?alt=media&token=97a55844-f077-4aeb-8402-e0a27221570b",
+//         "desc":"요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
+//         "date":"2020.09.09"
+//     })
+    
+//     useEffect(()=>{
+//         console.log(route)
+//         // console.log(navigation.navigate)
+
+//         //Card.js에서 navigation.navigate 함수를 쓸때 두번째 인자로 content를 넘겨줬죠?
+//   //content는 딕셔너리 그 자체였으므로 route.params에 고대~로 남겨옵니다.
+//   //즉, route.params 는 content죠!
+
+//         navigation.setOptions({
+//                                                 //setOptions로 페이지 타이틀도 지정 가능하고
+//             title:route.params.title,
+//                                                 //StackNavigator에서 작성했던 옵션을 다시 수정할 수도 있습니다. 
+//             headerStyle: {
+//                 backgroundColor: '#000',
+//                 shadowColor: "#000",
+//             },
+//             headerTintColor: "#fff",
+//         })
+//         setTip(route.params)
+//     },[])
+
+//     const popup = () => {
+//         Alert.alert("팝업!!")
+//     }
+//     return ( 
+//         // ScrollView에서의 flex 숫자는 의미가 없습니다. 정확히 보여지는 화면을 몇등분 하지 않고
+//         // 화면에 넣은 컨텐츠를 모두 보여주려 스크롤 기능이 존재하기 때문입니다. 
+//         // 여기선 내부의 컨텐츠들 영역을 결정짓기 위해서 height 값과 margin,padding 값을 적절히 잘 이용해야 합니다. 
+//         <ScrollView style={styles.container}>
+//             <Image style={styles.image} source={{uri:tip.image}}/>
+//             <View style={styles.textContainer}>
+//                 <Text style={styles.title}>{tip.title}</Text>
+//                 <Text style={styles.desc}>{tip.desc}</Text>
+//                 <TouchableOpacity style={styles.button} onPress={()=>popup()}><Text style={styles.buttonText}>팁 찜하기</Text></TouchableOpacity>
+//             </View>
+            
+//         </ScrollView>
+    
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container:{
+//         backgroundColor:"#000"
+//     },
+//     image:{
+//         height:400,
+//         margin:10,
+//         marginTop:40,
+//         borderRadius:20
+//     },
+//     textContainer:{
+//         padding:20,
+//         justifyContent:'center',
+//         alignItems:'center'
+//     },
+//     title: {
+//         fontSize:20,
+//         fontWeight:'700',
+//         color:"#eee"
+//     },
+//     desc:{
+//         marginTop:10,
+//         color:"#eee"
+//     },
+//     button:{
+//         width:100,
+//         marginTop:20,
+//         padding:10,
+//         borderWidth:1,
+//         borderColor:'deeppink',
+//         borderRadius:7
+//     },
+//     buttonText:{
+//         color:'#fff',
+//         textAlign:'center'
+//     }
+// })
+
+// import React,{useState,useEffect} from 'react';
+// import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity,Alert,Share } from 'react-native';
+
+// export default function Detail({navigation,route}) {
+
+//     const [tip, setTip] = useState({
+//         "idx":9,
+//         "category":"재테크",
+//         "title":"렌탈 서비스 금액 비교해보기",
+//         "image": "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Frental.png?alt=media&token=97a55844-f077-4aeb-8402-e0a27221570b",
+//         "desc":"요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
+//         "date":"2020.09.09"
+//     })
+    
+//     useEffect(()=>{
+//         console.log(route)
+//         navigation.setOptions({
+//             title:route.params.title,
+//             headerStyle: {
+//                 backgroundColor: '#000',
+//                 shadowColor: "#000",
+//             },
+//             headerTintColor: "#fff",
+//         })
+//         setTip(route.params)
+//     },[])
+
+//     const popup = () => {
+//         Alert.alert("팝업!!")
+//     }
+
+//     const share = () => {
+//         Share.share({
+//             message:`${tip.title} \n\n ${tip.desc} \n\n ${tip.image}`,
+//         });
+//     }
+//     return ( 
+//         // ScrollView에서의 flex 숫자는 의미가 없습니다. 정확히 보여지는 화면을 몇등분 하지 않고
+//         // 화면에 넣은 컨텐츠를 모두 보여주려 스크롤 기능이 존재하기 때문입니다. 
+//         // 여기선 내부의 컨텐츠들 영역을 결정짓기 위해서 height 값과 margin,padding 값을 적절히 잘 이용해야 합니다. 
+//         <ScrollView style={styles.container}>
+//             <Image style={styles.image} source={{uri:tip.image}}/>
+//             <View style={styles.textContainer}>
+//                 <Text style={styles.title}>{tip.title}</Text>
+//                 <Text style={styles.desc}>{tip.desc}</Text>
+//                 <View style={styles.buttonGroup}>
+//                     <TouchableOpacity style={styles.button} onPress={()=>popup()}><Text style={styles.buttonText}>팁 찜하기</Text></TouchableOpacity>
+//                     <TouchableOpacity style={styles.button} onPress={()=>share()}><Text style={styles.buttonText}>팁 공유하기</Text></TouchableOpacity>
+//                 </View>
+                
+//             </View>
+            
+//         </ScrollView>
+    
+//     )
+// }
+
+// const styles = StyleSheet.create({
+//     container:{
+//         backgroundColor:"#000"
+//     },
+//     image:{
+//         height:400,
+//         margin:10,
+//         marginTop:40,
+//         borderRadius:20
+//     },
+//     textContainer:{
+//         padding:20,
+//         justifyContent:'center',
+//         alignItems:'center'
+//     },
+//     title: {
+//         fontSize:20,
+//         fontWeight:'700',
+//         color:"#eee"
+//     },
+//     desc:{
+//         marginTop:10,
+//         color:"#eee"
+//     },
+//     buttonGroup: {
+//         flexDirection:"row",
+//     },
+//     button:{
+//         width:100,
+//         marginTop:20,
+//         marginRight:10,
+//         marginLeft:10,
+//         padding:10,
+//         borderWidth:1,
+//         borderColor:'deeppink',
+//         borderRadius:7
+//     },
+//     buttonText:{
+//         color:'#fff',
+//         textAlign:'center'
+//     }
+// })
+
+import React,{useState,useEffect} from 'react';
+import { StyleSheet, Text, View, Image, ScrollView,TouchableOpacity,Alert,Share } from 'react-native';
+import * as Linking from 'expo-linking';
+
+export default function Detail({navigation,route}) {
+
+    const [tip, setTip] = useState({
         "idx":9,
         "category":"재테크",
         "title":"렌탈 서비스 금액 비교해보기",
         "image": "https://firebasestorage.googleapis.com/v0/b/sparta-image.appspot.com/o/lecture%2Frental.png?alt=media&token=97a55844-f077-4aeb-8402-e0a27221570b",
         "desc":"요즘은 정수기, 공기 청정기, 자동차나 장난감 등 다양한 대여서비스가 활발합니다. 사는 것보다 경제적이라고 생각해 렌탈 서비스를 이용하는 분들이 늘어나고 있는데요. 다만, 이런 렌탈 서비스 이용이 하나둘 늘어나다 보면 그 금액은 겉잡을 수 없이 불어나게 됩니다. 특히, 렌탈 서비스는 빌려주는 물건의 관리비용까지 포함된 것이기에 생각만큼 저렴하지 않습니다. 직접 관리하며 사용할 수 있는 물건이 있는지 살펴보고, 렌탈 서비스 항목에서 제외해보세요. 렌탈 비용과 구매 비용, 관리 비용을 여러모로 비교해보고 고민해보는 것이 좋습니다. ",
         "date":"2020.09.09"
-    };
+    })
+    
+    useEffect(()=>{
+        console.log(route)
+        navigation.setOptions({
+            title:route.params.title,
+            headerStyle: {
+                backgroundColor: '#000',
+                shadowColor: "#000",
+            },
+            headerTintColor: "#fff",
+        })
+        setTip(route.params)
+    },[])
 
-    function toggleColor() {
-        // styles.container.backgroundColor === { color: "white" } ? { color: "black" } : { color: "white" };
-        return null;
+    const popup = () => {
+        Alert.alert("팝업!!")
     }
 
-    return (
-        <ScrollView contentContainerStyle={styles.container}>
-            <Image 
-                source={{uri: tip.image}}
-                style={styles.mainImage}
-            />
-            <Text style={Object.assign({}, styles.detailTitle, toggleColor())}>{tip.title}</Text>
-            {/* <Text style={styles.detailTitle}>{styles.container.backgroundColor}</Text> */}
-            <Text style={styles.detailDescription}>{tip.desc}</Text>
-            <TouchableOpacity style={styles.tipButton}><Text style={styles.tipButtonText}>팁 찜하기</Text></TouchableOpacity>
+    const share = () => {
+        Share.share({
+            message:`${tip.title} \n\n ${tip.desc} \n\n ${tip.image}`,
+        });
+    }
+
+    const link = () => {
+        Linking.openURL("https://spartacodingclub.kr")
+    }
+    return ( 
+        // ScrollView에서의 flex 숫자는 의미가 없습니다. 정확히 보여지는 화면을 몇등분 하지 않고
+        // 화면에 넣은 컨텐츠를 모두 보여주려 스크롤 기능이 존재하기 때문입니다. 
+        // 여기선 내부의 컨텐츠들 영역을 결정짓기 위해서 height 값과 margin,padding 값을 적절히 잘 이용해야 합니다. 
+        <ScrollView style={styles.container}>
+            <Image style={styles.image} source={{uri:tip.image}}/>
+            <View style={styles.textContainer}>
+                <Text style={styles.title}>{tip.title}</Text>
+                <Text style={styles.desc}>{tip.desc}</Text>
+                <View style={styles.buttonGroup}>
+                    <TouchableOpacity style={styles.button} onPress={()=>popup()}><Text style={styles.buttonText}>팁 찜하기</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={()=>share()}><Text style={styles.buttonText}>팁 공유하기</Text></TouchableOpacity>
+                    <TouchableOpacity style={styles.button} onPress={()=>link()}><Text style={styles.buttonText}>외부 링크</Text></TouchableOpacity>
+                </View>
+                
+            </View>
+            
         </ScrollView>
-    );
+    
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        flex: 1,
+    container:{
+        backgroundColor:"#000"
     },
-    mainImage: {
-        width: "100%",
-        height: "50%",
-        marginTop: 22,
+    image:{
+        height:400,
+        margin:10,
+        marginTop:40,
+        borderRadius:20
     },
-    detailTitle: {
-        // color: "black",
-        alignSelf: "center",
-        fontSize: 22,
-        fontWeight: "700",
-        margin: 22,
+    textContainer:{
+        padding:20,
+        justifyContent:'center',
+        alignItems:'center'
     },
-    detailDescription: {
-        // color: "black",
-        width: "90%",
-        alignSelf: "center",
-        lineHeight: 20,
+    title: {
+        fontSize:20,
+        fontWeight:'700',
+        color:"#eee"
     },
-    tipButton: {
-        borderColor: "pink",
-        borderWidth: 1,
-        borderStyle: "solid",
-        width: "28%",
-        alignSelf: "center",
-        margin: 28,
-        borderRadius: 5
+    desc:{
+        marginTop:10,
+        color:"#eee"
     },
-    tipButtonText: {
-        // color: "black",
-        textAlign: "center",
-        padding: 10
+    buttonGroup: {
+        flexDirection:"row",
     },
-
-});
-
+    button:{
+        width:90,
+        marginTop:20,
+        marginRight:10,
+        marginLeft:10,
+        padding:10,
+        borderWidth:1,
+        borderColor:'deeppink',
+        borderRadius:7
+    },
+    buttonText:{
+        color:'#fff',
+        textAlign:'center'
+    }
+})
--- components/note.js
+++ components/note.js
@@ -1,153 +1,258 @@
-import React from "react"
-import {View,Text,Image,StyleSheet} from "react-native";
+// import React from "react"
+// import {View,Text,Image,StyleSheet} from "react-native";
 
-//비구조 할당 방식으로 넘긴 속성 데이터를 꺼내 사용함
-export default function Note({content}) {
+// //비구조 할당 방식으로 넘긴 속성 데이터를 꺼내 사용함
+// export default function Note({content, navigation}) {
 
-  function oddEven(idx) {
-    const odd = "#f7f7f7";
-    if (!(idx % 2)) return { backgroundColor: odd, borderRadius: 5 }
-  }
+//   function oddEven(idx) {
+//     const odd = "#f7f7f7";
+//     if (!(idx % 2)) return { backgroundColor: odd, borderRadius: 5 }
+//   }
 
-    return (
-        <View style={Object.assign({}, styles.singleNoteContainer)}>
-            <View style={styles.pizzaContainer}>
-                <Image
-                // source={pizzaPhoto}
-                source={{uri: content.image}}
-                resizeMode="cover"
-                style={styles.pizzaImage}
-                />
+//     return (
+//         <View style={Object.assign({}, styles.singleNoteContainer)}>
+//             <View style={styles.pizzaContainer}>
+//                 <Image
+//                 // source={pizzaPhoto}
+//                 source={{uri: content.image}}
+//                 resizeMode="cover"
+//                 style={styles.pizzaImage}
+//                 />
+//             </View>
+//             <View style={styles.pizzaTextContainer}>
+//                 <Text h3 style={styles.pizzaTitle}>{content.title}</Text>
+//                 <Text numberOfLines={3} style={styles.pizzaDescription}>
+//                 {content.desc}
+//                 </Text>
+//                 <Text style={styles.noteDate}>{content.date}</Text>
+//             </View>
+//         </View>
+//     );
+// }
+
+
+
+// function pickColor() {
+//   const arr = ["#a5b6ee", "#d2a5ee", "#eea7a5", "#baeea5", "#ffedb3", "#adffcb", "#ffadbf", "#cbffad","#ffadad", "#a6a9fc", "#5abfb5"];
+//   // let randIdx = Math.floor(Math.random() * arr.length);
+//   return arr[Math.floor(Math.random() * arr.length)];
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     // backgroundColor:"#d9818f",
+//     paddingLeft: 15,
+//     paddingRight: 15,
+//   },
+//   textContainer: {
+//     // backgroundColor: "gray",
+//     justifyContent: "flex-end",
+//   },
+//   textHeader: {
+//     fontSize: 50,
+//     paddingBottom: 5,
+//     marginTop: 12,
+//     // paddingLeft: 8,
+//   },
+//   contentContainer: {
+//     // backgroundColor: "#adbae6"
+//   },
+//   imageContainer: {
+//     width: "100%",
+//     height: 180,
+//   },
+//   image: {
+//     width:"100%",
+//     height:"100%",
+//     borderRadius:20,
+//   },
+//   categoryContainer: {
+//     marginTop: 10,
+//     height: 60,
+//     flexDirection: "row",
+//     alignItems: "center",
+//   },
+//   categoryButtons1: {
+//     borderRadius: 15,
+//     height: 50,
+//     width: 100,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginRight: 15,
+//     backgroundColor: pickColor(),
+//   },
+//   categoryButtons2: {
+//     borderRadius: 15,
+//     height: 50,
+//     width: 100,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginRight: 15,
+//     backgroundColor: pickColor(),
+//   },
+//   categoryButtons3: {
+//     borderRadius: 15,
+//     height: 50,
+//     width: 100,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     marginRight: 15,
+//     backgroundColor: pickColor(),
+//   },
+//   categoryButtons4: {
+//     borderRadius: 15,
+//     height: 50,
+//     width: 100,
+//     alignItems: "center",
+//     justifyContent: "center",
+//     // marginRight: 50,
+//     backgroundColor: pickColor(),
+//   },
+//   categoryText: {
+//     color: "white",
+//     fontWeight: "700"
+//   },
+//   noteContainer: {
+//     flex: 6,
+//     marginTop: 10,
+//   },
+//   singleNoteContainer: {
+//     flex: 1,
+//     flexDirection: "row",
+//   },
+//   pizzaContainer: {
+//     flex: 1,
+//     paddingBottom: 10,
+//   }, 
+//   pizzaImage: {
+//     height:"100%",
+//     width:"100%",
+//     borderRadius: 10,
+//     marginTop: 5,
+//     marginLeft: 5,
+//   },
+//   pizzaTextContainer: {
+//     flex: 2,
+//     paddingTop: 5,
+//     paddingLeft: 15,
+//     paddingRight: 10,
+//     paddingBottom: 10,
+//   },
+//   pizzaText: {
+//     flex:2,
+//   },
+//   pizzaTitle: {
+//     fontWeight: "800",
+//     paddingBottom: 5,
+//   },
+//   noteDate: {
+//     color: "#d9d9d9",
+//     fontSize: 13,
+//     paddingTop: 5,
+//   },
+// });
+
+// import React from 'react';
+// import {View, Image, Text, StyleSheet,TouchableOpacity} from 'react-native'
+
+// //MainPage로 부터 navigation 속성을 전달받아 Card 컴포넌트 안에서 사용
+// export default function Note({content,navigation}){
+//     return(
+//         //카드 자체가 버튼역할로써 누르게되면 상세페이지로 넘어가게끔 TouchableOpacity를 사용
+//         <TouchableOpacity style={styles.card} onPress={()=>{navigation.navigate('Detail')}}>
+//             <Image style={styles.cardImage} source={{uri:content.image}}/>
+//             <View style={styles.cardText}>
+//                 <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
+//                 <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
+//                 <Text style={styles.cardDate}>{content.date}</Text>
+//             </View>
+//         </TouchableOpacity>
+//     )
+// }
+
+// const styles = StyleSheet.create({
+    
+//     card:{
+//       flex:1,
+//       flexDirection:"row",
+//       margin:10,
+//       borderBottomWidth:0.5,
+//       borderBottomColor:"#eee",
+//       paddingBottom:10
+//     },
+//     cardImage: {
+//       flex:1,
+//       width:100,
+//       height:100,
+//       borderRadius:10,
+//     },
+//     cardText: {
+//       flex:2,
+//       flexDirection:"column",
+//       marginLeft:10,
+//     },
+//     cardTitle: {
+//       fontSize:20,
+//       fontWeight:"700"
+//     },
+//     cardDesc: {
+//       fontSize:15
+//     },
+//     cardDate: {
+//       fontSize:10,
+//       color:"#A6A6A6",
+//     }
+// });
+
+import React from 'react';
+import {View, Image, Text, StyleSheet,TouchableOpacity} from 'react-native'
+
+//MainPage로 부터 navigation 속성을 전달받아 Card 컴포넌트 안에서 사용
+export default function Note({content,navigation}){
+    return(
+        //카드 자체가 버튼역할로써 누르게되면 상세페이지로 넘어가게끔 TouchableOpacity를 사용
+        <TouchableOpacity style={styles.card} onPress={()=>{navigation.navigate('Detail', content)}}>
+            <Image style={styles.cardImage} source={{uri:content.image}}/>
+            <View style={styles.cardText}>
+                <Text style={styles.cardTitle} numberOfLines={1}>{content.title}</Text>
+                <Text style={styles.cardDesc} numberOfLines={3}>{content.desc}</Text>
+                <Text style={styles.cardDate}>{content.date}</Text>
             </View>
-            <View style={styles.pizzaTextContainer}>
-                <Text h3 style={styles.pizzaTitle}>{content.title}</Text>
-                <Text numberOfLines={3} style={styles.pizzaDescription}>
-                {content.desc}
-                </Text>
-                <Text style={styles.noteDate}>{content.date}</Text>
-            </View>
-        </View>
-    );
+        </TouchableOpacity>
+    )
 }
 
-
-
-function pickColor() {
-  const arr = ["#a5b6ee", "#d2a5ee", "#eea7a5", "#baeea5", "#ffedb3", "#adffcb", "#ffadbf", "#cbffad","#ffadad", "#a6a9fc", "#5abfb5"];
-  // let randIdx = Math.floor(Math.random() * arr.length);
-  return arr[Math.floor(Math.random() * arr.length)];
-}
 
 const styles = StyleSheet.create({
-  container: {
-    // backgroundColor:"#d9818f",
-    paddingLeft: 15,
-    paddingRight: 15,
-  },
-  textContainer: {
-    // backgroundColor: "gray",
-    justifyContent: "flex-end",
-  },
-  textHeader: {
-    fontSize: 50,
-    paddingBottom: 5,
-    marginTop: 12,
-    // paddingLeft: 8,
-  },
-  contentContainer: {
-    // backgroundColor: "#adbae6"
-  },
-  imageContainer: {
-    width: "100%",
-    height: 180,
-  },
-  image: {
-    width:"100%",
-    height:"100%",
-    borderRadius:20,
-  },
-  categoryContainer: {
-    marginTop: 10,
-    height: 60,
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  categoryButtons1: {
-    borderRadius: 15,
-    height: 50,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 15,
-    backgroundColor: pickColor(),
-  },
-  categoryButtons2: {
-    borderRadius: 15,
-    height: 50,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 15,
-    backgroundColor: pickColor(),
-  },
-  categoryButtons3: {
-    borderRadius: 15,
-    height: 50,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    marginRight: 15,
-    backgroundColor: pickColor(),
-  },
-  categoryButtons4: {
-    borderRadius: 15,
-    height: 50,
-    width: 100,
-    alignItems: "center",
-    justifyContent: "center",
-    // marginRight: 50,
-    backgroundColor: pickColor(),
-  },
-  categoryText: {
-    color: "white",
-    fontWeight: "700"
-  },
-  noteContainer: {
-    flex: 6,
-    marginTop: 10,
-  },
-  singleNoteContainer: {
-    flex: 1,
-    flexDirection: "row",
-  },
-  pizzaContainer: {
-    flex: 1,
-    paddingBottom: 10,
-  }, 
-  pizzaImage: {
-    height:"100%",
-    width:"100%",
-    borderRadius: 10,
-    marginTop: 5,
-    marginLeft: 5,
-  },
-  pizzaTextContainer: {
-    flex: 2,
-    paddingTop: 5,
-    paddingLeft: 15,
-    paddingRight: 10,
-    paddingBottom: 10,
-  },
-  pizzaText: {
-    flex:2,
-  },
-  pizzaTitle: {
-    fontWeight: "800",
-    paddingBottom: 5,
-  },
-  noteDate: {
-    color: "#d9d9d9",
-    fontSize: 13,
-    paddingTop: 5,
-  },
+    
+    card:{
+      flex:1,
+      flexDirection:"row",
+      margin:10,
+      borderBottomWidth:0.5,
+      borderBottomColor:"#eee",
+      paddingBottom:10
+    },
+    cardImage: {
+      flex:1,
+      width:100,
+      height:100,
+      borderRadius:10,
+    },
+    cardText: {
+      flex:2,
+      flexDirection:"column",
+      marginLeft:10,
+    },
+    cardTitle: {
+      fontSize:20,
+      fontWeight:"700"
+    },
+    cardDesc: {
+      fontSize:15
+    },
+    cardDate: {
+      fontSize:10,
+      color:"#A6A6A6",
+    }
 });
